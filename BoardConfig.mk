8edb89dd6348c09eb0d0ccff78090ffffb1a3f11
Annnekkk <trnhnhoa@gmail.com>
2026-04-20 21:45:51 +0700

    Test with disable encryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,8 @@ TARGET_NO_RADIOIMAGE := true
 # Crypto
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
+#TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE := true
 
 # Kernel
